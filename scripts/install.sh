ad1086265d667ccd561c9d566cfe4540a8a7a6bf
Shanda Lau <shandalaulv@gmail.com>
2022-02-16 10:28:40 +0800

    update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,2 +1,4 @@
-pip install -r requriement.txt
+conda create -n fgcv python=3.7
+conda activate -n fgcv
+pip install -r requriement.txt  -i http://mirrors.aliyun.com/pypi/simple --trusted-host mirrors.aliyun.com
 conda install -c conda-forge nvidia-apex
